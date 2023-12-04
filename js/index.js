427992f70624d173bce8a8719c18f57b6c3548e0
ichsan <34944169+icanq@users.noreply.github.com>
2023-12-04 21:10:29 +0700

    Update index.js
    change api_url to the new one

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://capstone-be-icanq-production.up.railway.app"
 
 document.addEventListener("DOMContentLoaded", async () => {
 	if (window.location.pathname.includes("product.html")) {
